75a6cb7e32d343a22e2d4653e1700e9cfaea8bf6
Ayrton Souza <ayrtonsouza@icloud.com>
2022-06-23 00:50:39 -0300

    [:test_tube:] creating place order method test in order service

---
+++ src/entities/index.ts
@@ -0,0 +1,5 @@
+export * from './address';
+export * from './customer';
+export * from './order';
+export * from './order_item';
+export * from './product';
--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,13 +1,46 @@
-import { Order } from '../entities/order';
-import { OrderItem } from '../entities/order_item';
+import { Customer, Order, OrderItem } from '../entities';
 import { OrderService } from './order.service';
 
 describe('Order Service', () => {
+  it('should be able to place a new order', () => {
+    const customer = new Customer('1234567890', 'John Doe');
+    const item = new OrderItem('1', '1234567890', 'First Item', 100, 2);
+
+    const order = new OrderService.placeOrder(customer, [item]);
+
+    expect(customer.rewardPoints).toBe(100);
+    expect(order.total()).toBe(200);
+  });
+
   it('should be able to summarize all orders value', () => {
-    const item1 = new OrderItem('1234567890', '1234567890', 'Item 1', 100, 1);
-    const item2 = new OrderItem('0987654321', '0987654321', 'Item 2', 200, 2);
-    const item3 = new OrderItem('1234567890', '1234567890', 'Item 1', 150, 1);
-    const item4 = new OrderItem('0987654321', '0987654321', 'Item 2', 120, 2);
+    const item1 = new OrderItem(
+      '1234567890',
+      '1234567890',
+      'First Item',
+      100,
+      1
+    );
+    const item2 = new OrderItem(
+      '0987654321',
+      '0987654321',
+      'Second Item',
+      200,
+      2
+    );
+    const item3 = new OrderItem(
+      '1234567890',
+      '1234567890',
+      'Third Item',
+      150,
+      1
+    );
+    const item4 = new OrderItem(
+      '0987654321',
+      '0987654321',
+      'Fourth Item',
+      120,
+      2
+    );
 
     const order1 = new Order('123456', '123456', [item1, item2]);
     const order2 = new Order('098765', '098765', [item3, item4]);
