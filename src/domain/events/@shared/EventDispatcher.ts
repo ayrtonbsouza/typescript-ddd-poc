ca6e024b30540b38d4d738f149fc72e36cab0f45
Ayrton Souza <ayrtonsouza@icloud.com>
2022-06-26 23:19:11 -0300

    [:white_check_mark:] making register event test pass

--- src/domain/events/@shared/EventDispatcher.ts
+++ src/domain/events/@shared/EventDispatcher.ts
@@ -3,15 +3,27 @@ import { IEventDispatcher } from './IEventDispatcher';
 import { IEventHandler } from './IEventHandler';
 
 export class EventDispatcher implements IEventDispatcher {
+  private eventHandlers: { [eventName: string]: IEventHandler<IEvent>[] } = {};
+
+  get getEventHandlers(): { [eventName: string]: IEventHandler[] } {
+    return this.eventHandlers;
+  }
+
   notify(event: IEvent): void {
     throw new Error('Method not implemented.');
   }
+
   register(eventName: string, eventHandler: IEventHandler<IEvent>): void {
-    throw new Error('Method not implemented.');
+    if (!this.eventHandlers[eventName]) {
+      this.eventHandlers[eventName] = [];
+    }
+    this.eventHandlers[eventName].push(eventHandler);
   }
+
   unregister(eventName: string, eventHandler: IEventHandler<IEvent>): void {
     throw new Error('Method not implemented.');
   }
+
   unregisterAll(): void {
     throw new Error('Method not implemented.');
   }
